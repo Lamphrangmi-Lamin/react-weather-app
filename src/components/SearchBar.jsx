582e60b2e2ce443f7dc1bd456bb797ac750aed3d
Lamphrangmi-Lamin <lamphrangmilamin46169@gmail.com>
2025-02-24 07:32:47 +0530

    adding assets and SearchBar component

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./SearchBar.css";
+function SearchBar() {
+    return (
+        <div>
+            <input
+              type="text"
+              placeholder="Search city"
+            //   value={value}
+              onChange={""}
+            />
+        </div>
+    )
+}
+
+export default SearchBar;
